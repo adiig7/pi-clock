37f9af203b150b7b1b82e73e7e7c6fc23634130c
Aditya Gupta <adityagupta765469@gmail.com>
2024-10-12 21:55:24 +0530

    [UPDATE] components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <h1 className="text-center text-white mb-4 text-7xl">&#960; clock</h1>
-  )
-}
+    <>
+      <h1 className="text-center text-white text-7xl">&#960; clock</h1>
+      <p className="text-center text-lg">The clock is powered by the decimal digits of the PI value</p>
+    </>
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/components/PI.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect, useRef } from "react";
+import { pi } from "./config";
+
+const PI = () => {
+  const [time, setTime] = useState("");
+  const [highlightedPi, setHighlightedPi] = useState(pi);
+  const highlightRef = useRef(null);
+
+  const updateTime = () => {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    setTime(`${hours}${minutes}`);
+  };
+
+  useEffect(() => {
+    updateTime();
+    const intervalId = setInterval(updateTime, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {
+    const firstIndex = pi.indexOf(time);
+    if (firstIndex !== -1) {
+      const before = pi.substring(0, firstIndex);
+      const after = pi.substring(firstIndex + time.length);
+      setHighlightedPi(
+        <>
+          {before}
+          <span
+            ref={highlightRef}
+            className="bg-red-500 text-white text-3xl tracking-widest px-2 mx-2 rounded-md"
+          >
+            {time.slice(0, 2)}:{time.slice(2)}
+          </span>
+          {after}
+        </>
+      );
+
+      setTimeout(() => {
+        highlightRef.current?.scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+        });
+      }, 0);
+    } else {
+      setHighlightedPi(pi);
+    }
+  }, [time]);
+
+  return (
+    <p className="break-words text-gray-500 text-center text-sm md:text-xl tracking-widest select-none">
+      {highlightedPi}
+    </p>
+  );
+};
+
+export default PI;
